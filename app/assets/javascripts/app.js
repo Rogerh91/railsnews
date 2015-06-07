2bf18fb4dc882c936fdfd04b9b8231baf93d60e7
Rogerh91 <roger.huang@thoughtbasin.com>
2015-06-07 18:51:39 -0400

    More fancy things
    woots

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,14 +1,15 @@
-angular.module('railsnews', [])
+angular.module('railsnews', ['ui.router'])
+.factory('posts', [function(){
+	var o = {
+		posts: []
+	};
+	return o;
+}])
 .controller('MainCtrl', [
 '$scope',
-function($scope){
-	$scope.posts = [
-		{title: 'post 1', votes: 5},
-		{title: 'post 2', votes: 7},
-		{title: 'post 3', votes: 2},
-		{title: 'post 4', votes: 4},
-		{title: 'post 5', votes: 3}
-		];
+'posts',
+function($scope, posts){
+	$scope.posts = posts.posts;
 
 	$scope.addPost = function(){
 			$scope.posts.push({
