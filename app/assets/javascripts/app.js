87c035d6b9ce875f663685ac49c4e9d8933d9f07
Rogerh91 <roger.huang@thoughtbasin.com>
2015-06-06 23:28:45 -0400

    Added some angular
    whoo

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,26 @@
+angular.module('railsnews', [])
+.controller('MainCtrl', [
+'$scope',
+function($scope){
+	$scope.posts = [
+		{title: 'post 1', votes: 5},
+		{title: 'post 2', votes: 7},
+		{title: 'post 3', votes: 2},
+		{title: 'post 4', votes: 4},
+		{title: 'post 5', votes: 3}
+		];
+
+	$scope.addPost = function(){
+			$scope.posts.push({title: $scope.title, votes: 1})
+			$scope.title ='';
+		};
+
+	$scope.removePost = function(){
+			$scope.posts.pop($scope.posts);
+		};
+
+	$scope.incrementUpvotes = function(post) {
+		post.votes += 1;
+	}
+
+	}]);
