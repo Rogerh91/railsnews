4fe6b6e59c920baaafb2d3a088d04077af3e7f50
Rogerh91 <roger.huang@thoughtbasin.com>
2015-06-27 01:06:21 -0400

    Angular-Rails!
    whoots

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,10 +1,33 @@
+//= require angular-resource
 angular.module('railsnews', ['ui.router'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+	$stateProvider
+	.state('home',{
+		url: '/home',
+		templateUrl: '/home.html',
+		controller: 'MainCtrl'
+	})
+
+	.state('posts', {
+		url: '/posts.{id}',
+		templateUrl: '/posts.html',
+		controller: 'PostsCtrl'
+	});
+
+	$urlRouterProvider.otherwise('home');
+}])
+
 .factory('posts', [function(){
 	var o = {
 		posts: []
 	};
 	return o;
 }])
+
 .controller('MainCtrl', [
 '$scope',
 'posts',
@@ -15,10 +38,16 @@ function($scope, posts){
 			$scope.posts.push({
 				title: $scope.title, 
 				link: $scope.link,
-				votes: 1})
+				votes: 1,
+				comments: [
+				{author: 'Roger', body: 'Yay!', votes:1},
+				{author: 'Bob', body: 'I am a plumber!',votes:1}
+				]
+			})
+
 			$scope.title ='';
 			$scope.link ='';
-		};
+	};
 
 	$scope.removePost = function(){
 			$scope.posts.pop($scope.posts);
@@ -28,4 +57,14 @@ function($scope, posts){
 		post.votes += 1;
 	}
 
-	}]);
+	}])
+
+.controller('PostsCtrl', [
+	'$scope',
+	'$stateParams',
+	'posts',
+	function($scope,$stateParams,posts){
+			$scope.post = posts.posts[$stateParams.id];
+}]);
+
+
